65503bd875a1ae2ffc3837844945486f3a306468
Timmifixedit <tim@luchterhand.org>
2023-05-19 18:04:54 +0200

    removed unused examples

--- examples/slides/optional.cpp
+++
@@ -1,5 +0,0 @@
-std::optional<int> res = calculateIfPossible();
-
-if (res.has_value()) {
-    std::cout << res.value() << std::endl;
-}
--- examples/slides/trailingreturn.cpp
+++
@@ -1,5 +0,0 @@
-// Normale Syntax
-std::vector<std::set<double>> a(double b) {
-
-// Trailing return-type
-auto a(double b) -> std::vector<std::set<double>> {
